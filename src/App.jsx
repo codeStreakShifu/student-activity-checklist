39aae502ce4d91d4d53cb3f1669739004047ef95
Your Name <undertaker0000aw@gmail.com>
2025-11-02 05:17:01 +0800

    fixing small bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
-
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import LandingPage from './components/LandingPage';
 import StudentDashboard from './components/StudentDashboard';
 import TeacherDashboard from './components/TeacherDashboard';
 
 export default function App() {
   return (
-    <Router>
-   
+    <Router basename="/student-activity-checklist">
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/student-dashboard" element={<StudentDashboard />} />
         <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
       </Routes>
-    
     </Router>
   );
 }
